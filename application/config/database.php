b7f50e1d3ad9d05063171890f7cc4d2299dd557c
psudocode <dhidyawdiyan@gmail.com>
2013-12-20 20:39:10 +0700

    Clone To Ubuntu

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'qwertyui';
 $db['default']['database'] = 'idtut';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
