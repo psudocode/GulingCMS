169d1b556cddab1f2a6fb265a3e05c3f50e7c136
psudocode <dhidyawdiyan@gmail.com>
2014-01-02 18:04:57 +0700

    Adding Comment _ part 1

---
+++ application/models/Comment.php
@@ -0,0 +1,3 @@
+<?php
+
+class Comment extends ActiveRecord\Model { }
--- application/views/single.php
+++ application/views/single.php
@@ -1,13 +1,13 @@
 <?php
-	$segment = 1;
-	$total = $this->uri->total_segments();
-        $base = substr(base_url(), 0, -1);
-	echo '<ol class="breadcrumb"><li><a href="'.base_url().'">home</a></li>';
+$segment = 1;
+$total = $this->uri->total_segments();
+$base = substr(base_url(), 0, -1);
+echo '<ol class="breadcrumb"><li><a href="' . base_url() . '">home</a></li>';
 ?>
 
 <?php while ($segment <= $total) : ?>
-    <?php echo '<li><a href="'.$base.'/'.$this->uri->segment($segment).'">'.$this->uri->segment($segment).'</a></li>'; ?>
-    <?php $base = $base.'/'.$this->uri->segment($segment); ?>
+    <?php echo '<li><a href="' . $base . '/' . $this->uri->segment($segment) . '">' . $this->uri->segment($segment) . '</a></li>'; ?>
+    <?php $base = $base . '/' . $this->uri->segment($segment); ?>
     <?php $segment++; ?>
 <?php endwhile; ?>
 <?php echo '</ol>'; ?>
@@ -21,7 +21,7 @@
 
                     <?php if ($post->image_feature) : ?>
                         <div class="panel-heading" style="overflow: hidden; height: 300px;">
-                            <img class="img-post-feature" src="<?=base_url()?><?=$post->image_feature?>" width="100%" />
+                            <img class="img-post-feature" src="<?= base_url() ?><?= $post->image_feature ?>" width="100%" />
                         </div>
                     <?php endif; ?>
 
@@ -47,13 +47,13 @@
                             <span class="small under-categories label label-info">
                                 <span class="category-titles">Categories :</span>
                                 <?php foreach ($cats as $cat) : ?>
-                                <a href="<?=  base_url('web/categories/'.Category::find($cat->category_id)->slug) ?>"><?php echo Category::find($cat->category_id)->title; ?></a> 
+                                    <a href="<?= base_url('web/categories/' . Category::find($cat->category_id)->slug) ?>"><?php echo Category::find($cat->category_id)->title; ?></a> 
                                 <?php endforeach; ?>
                             </span><br />
                         <?php endif; ?>
 
 
-                        
+
 
 
 
@@ -65,7 +65,7 @@
                             <span class="small with-tags label label-info">
                                 <span class="category-titles">Tags :</span>
                                 <?php foreach ($tags as $tag) : ?>
-                                    <a href="<?=  base_url('web/tags/'.Tag::find($tag->tag_id)->slug) ?>"><?php echo Tag::find($tag->tag_id)->title; ?></a> 
+                                    <a href="<?= base_url('web/tags/' . Tag::find($tag->tag_id)->slug) ?>"><?php echo Tag::find($tag->tag_id)->title; ?></a> 
                                 <?php endforeach; ?>   
                             </span>
                         <?php endif; ?>
@@ -99,6 +99,29 @@
 
                     </div>
                 </div>
+
+                <div class="row">
+                    <?php 
+                    $comments = Comment::find('all', array('post_id' => $post->id));
+                    ?>
+                    <?php foreach($comments as $comment) : ?>
+                    <div class="comment-loop">
+                        <div class="col-lg-2 visible-lg">
+                            <?php
+                            $comment_author_email_a = 'dhidyawdiyan@yahoo.co.id';
+                            $gravatar_link = 'http://www.gravatar.com/avatar/' . md5($comment_author_email_a) . '?s=80';
+                            echo '<img class="graphoto_c" src="' . $gravatar_link . '" />';
+                            ?>             
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="comment-content col-lg-10">
+                            <a href=""><strong>Awdiyan</strong></a>
+                            <div class="box-comment arrow_box">test Post Komentar</div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+
             </div>
             <!-- END :: Posting Loops -->
         <?php endif; ?>
